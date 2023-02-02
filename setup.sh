b19bfd366204d9c2755a00db3ee7850d25b2fb39
SIFR0X <sifr0x@protonmail.com>
2023-02-02 13:06:31 +0100

    add tmux to setup script

--- setup.sh
+++ setup.sh
@@ -9,10 +9,13 @@ EOF
 source ${HOME}/.sifr0x_dotfiles
 mkdir -p ~/.config
 
-
 # neovim
 NVIM_PATH="$DOTFILES_BASE_DIR/nvim"
 ln -s $NVIM_PATH ~/.config/nvim
+
+# tmux
+TMUX_CONF="$DOTFILES_BASE_DIR/tmux/tmux.conf"
+ln -s $TMUX_CONF ~/.tmux.conf
 
 # zsh
 cat <<EOF >> ${HOME}/.zshrc
